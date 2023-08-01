3c3f96c4bcf8f425769a035dae64cd4af8d05ada
Kent Ng <kent.ng201@gmail.com>
2023-08-01 20:51:34 +0800

    add router, delete index tsx

---
+++ src/router/index.tsx
@@ -0,0 +1,13 @@
+
+import {
+    createBrowserRouter,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: null,
+    },
+]);
+
+export default router;
