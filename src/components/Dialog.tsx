b41019a2e0cb7918975a842aa5ee78bb7740baea
Kent Ng <kent.ng201@gmail.com>
2023-09-06 11:59:19 +0800

    add delete function and confirm dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+export type DialogProps = {
+    children?: JSX.Element | (JSX.Element | string | boolean | undefined | null)[] | null | string | undefined;
+    show: boolean;
+    onClose?: () => void;
+    onClick?: () => void;
+};
 
-function Dialog() {
-    const [show, setShow] = useState(true);
-
+function Dialog({
+    children,
+    show,
+    onClose,
+    onClick,
+}: DialogProps) {
     return <>
         {
-            show && <div className='dialog-backdrop' onClick={() => setShow(false)} />
+            show && <div className='absolute dialog-backdrop' onClick={() => onClose?.()} />
         }
-        <dialog 
-            open={show} 
-            className='rounded bg-white shadow-md w-1/2 h-1/2 z-20 mt-20' 
-            onClose={() => setShow(false)}
-            onClick={() => setShow(false)}
+        <dialog
+            open={show}
+            className='absolute rounded bg-white dark:bg-slate-700 shadow-md w-1/2 min-h-1/2 z-20 mt-20'
+            onClose={() => onClose?.()}
+            onClick={() => onClick?.()}
         >
-            Test
+            {children}
         </dialog>
     </>;
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,6 +8,9 @@ import { decrypt } from "src/helpers/encryption";
 import { Collection } from "src/models/Collection";
 import { Connection } from "src/models/Connection";
 import AsyncLock from 'async-lock';
+import { Delete16Filled, Dismiss16Filled } from "@ricons/fluent";
+import Dialog from "src/components/Dialog";
+import Button from "src/components/Button";
 
 const lock = new AsyncLock();
 const LOCK_KEY = 'model-query';
@@ -41,6 +44,9 @@ function HomePage() {
     const [editItem, setEditItem] = useState<any>();
     const [editKey, setEditKey] = useState<string>();
     const editInputRef = useRef<HTMLInputElement>(null);
+
+    // Delete item
+    const [deleteItem, setDeleteItem] = useState<any>();
 
     const navigate = useNavigate();
 
@@ -158,13 +164,60 @@ function HomePage() {
         return STRING_TEXT_COLOR_LIGHT;
     }
 
-    return (
+    const formatDeletingItem = (item: any) => {
+        const newItem = { ...item };
+        newItem.id = newItem.id.split('.').slice(1).join('.');
+        return JSON.stringify(newItem, null, 4);
+    };
+
+    return (<>
+        <Dialog show={!!deleteItem}>
+            <div className="p-4">
+                <div className="w-full flex justify-between">
+                    <div className="font-bold text-red-500">Permanent Delete Alert</div>
+                    <Dismiss16Filled className="mt-1 w-4 h-4 text-black dark:text-slate-400 cursor-pointer" onClick={() => {
+                        setDeleteItem(undefined);
+                    }} />
+                </div>
+                <div className="h-6"></div>
+                <div className="text-black dark:text-slate-400">Do you sure you want to continue to delete the following item?</div>
+                <div className="h-4"></div>
+                <div className="text-[13px] border border-slate-200 dark:border-slate-900 w-full h-auto p-4 rounded-md dark:bg-slate-800 bg-slate-100">
+                    <pre>{deleteItem && formatDeletingItem(deleteItem)}</pre>
+                </div>
+                <div className="h-8"></div>
+                <div className="w-full flex justify-end gap-4">
+                    <Button size="sm" type="text" color="grey" onClick={() => {
+                        setDeleteItem(undefined);
+                    }}>Cancel</Button>
+                    <Button size="sm" type="outline" color="red" onClick={async () => {
+                        await db.remove(deleteItem.id, deleteItem.rev).catch((err: any) => {
+                            setAlert(<Alert type="error" message={err.message}></Alert>);
+                            setShowAlert(true);
+                            setTimeout(() => {
+                                setAlert(undefined);
+                                setShowAlert(false);
+                            }, 4000);
+                        });
+                        setDeleteItem(undefined);
+                        await getModels(currentCollection as Collection);
+                        setAlert(<Alert type="success" message={'Deleted successfully'}></Alert>);
+                        setShowAlert(true);
+                        setTimeout(() => {
+                            setAlert(undefined);
+                            setShowAlert(false);
+                        }, 4000);
+
+                    }}>Confirm</Button>
+                </div>
+            </div>
+        </Dialog>
         <div className="w-screen h-screen bg-slate-100 dark:bg-slate-900 flex">
             {
                 showAlert && alert
             }
             <div className="w-1/6 p-4">
-                <div className="ml-2 mb-4 dark:text-white font-bold">{connection?.name || ''}</div>
+                <div className="ml-2 mb-4 dark:text-slate-400 font-bold">{connection?.name || ''}</div>
                 <input
                     className="w-full h-8 px-4 text-sm rounded-full dark:bg-slate-600 dark:placeholder:text-slate-500 shadow-sm focus:outline-none focus:border-blue-500"
                     placeholder="Search Collection"
@@ -189,7 +242,7 @@ function HomePage() {
                                     await getModels(item);
                                 }}
                             >
-                                <div className="text-slate-900 dark:text-white">{item.id}</div>
+                                <div className="text-slate-900 dark:text-slate-400">{item.id}</div>
                             </div>
                         })
                     }
@@ -198,12 +251,12 @@ function HomePage() {
             <div className="w-5/6 overflow-x-auto bg-white dark:bg-slate-800 shadow-sm rounded-lg p-4">
                 {
                     !filteredResults && <div className="w-full h-full flex justify-center items-center">
-                        <div className="text-slate-600 dark:text-white">Please select a collection</div>
+                        <div className="text-slate-600 dark:text-slate-400">Please select a collection</div>
                     </div>
                 }
                 <div className="w-full h-16">
                     <div className="absolute">
-                        <div className="p-2 pb-8 text-2xl dark:text-white">{currentCollection?.id || ''}</div>
+                        <div className="p-2 pb-8 text-2xl dark:text-slate-400">{currentCollection?.id || ''}</div>
                     </div>
                     <div className="overflow-x-auto">
                         <div className="mt-[48px]" style={{
@@ -213,13 +266,15 @@ function HomePage() {
                                 filteredResults ? <table>
                                     <thead>
                                         <tr>
+                                            <th></th>
                                             {
                                                 filteredResults ? attributes.map((key, index) => {
-                                                    return <th key={index} className="px-2 pb-2 text-slate-900 dark:text-white">{formatKey(key)}</th>
+                                                    return <th key={index} className="px-2 pb-2 text-slate-900 dark:text-slate-400">{formatKey(key)}</th>
                                                 }) : <></>
                                             }
                                         </tr>
                                         <tr>
+                                            <th></th>
                                             {
                                                 filteredResults ? attributes.map((key, index) => {
                                                     return <td key={index} className={"pl-1 pb-2 " + getWidth()}>
@@ -249,9 +304,14 @@ function HomePage() {
                                         {
                                             filteredResults ? filteredResults?.map((item, index) => {
                                                 return <tr key={index}>
+                                                    <td>
+                                                        <Delete16Filled className="mt-[-8px] w-4 h-4 text-red-500 cursor-pointer" onClick={() => {
+                                                            setDeleteItem(item);
+                                                        }} />
+                                                    </td>
                                                     {
                                                         attributes.map((key, indexJ) => {
-                                                            return <td key={indexJ} valign="top" className="px-2 pb-2 text-slate-900 text-xs dark:text-white">
+                                                            return <td key={indexJ} valign="top" className="px-2 pb-2 text-slate-900 text-xs dark:text-slate-400">
                                                                 {
                                                                     editItem && editItem.id === item.id && editKey === key ? <input
                                                                         ref={editInputRef}
@@ -357,7 +417,8 @@ function HomePage() {
                 </div>
             </div>
         </div>
-    );
+
+    </>);
 }
 
 export default HomePage;
